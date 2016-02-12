c9ea1a1ccd403307c723c83d9676fd9d91993f74
Yiruz <2yi.z.ru2@gmail.com>
2016-02-12 11:52:05 -0500

    add toString

--- 03KnightsTour/KnightBoard.java
+++ 03KnightsTour/KnightBoard.java
@@ -14,4 +14,16 @@ public class KnightBoard{
 	}
     }
 
+    public String toString(){
+	String ans="";
+	for(int r=2; r<board.length-2; r++){
+	    for(int c=2; c<board.length-2; c++){
+		ans+=board[r][c]+"\t";
+	    }
+	    ans+="\n";
+	}
+	return ans;
+    }
+
+    
 }
