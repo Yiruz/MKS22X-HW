10202c6ba74c05f6e1669e2c71c541d298387776
Yiruz <2yi.z.ru2@gmail.com>
2016-02-14 11:48:55 -0500

    fix backtracking problem

--- 03KnightsTour/KnightBoard.java
+++ 03KnightsTour/KnightBoard.java
@@ -4,15 +4,15 @@ public class KnightBoard{
     public KnightBoard(int size){
 	board = new int [size+4][size+4];
 	for(int r=0; r<board.length; r++){
-	    for(int c=0; c<board.length; c++){
-		if(r<2 || r>board.length-3 || c<2 || c>board.length-3){
+	    for(int c=0; c<board[r].length; c++){
+		if(r<2 || r>board.length-3 || c<2 || c>board[r].length-3){
 		    board[r][c]=-1;
 		}else{
 		    board[r][c]=0;
 		}
 	    }
 	}
-	}
+    }
     /*public KnightBoard(int size){
 	board = new int[size][size];
 	}*/
@@ -21,15 +21,17 @@ public class KnightBoard{
 	return solveH(2,2,1);
     }
     public boolean solveH(int r, int c, int step){
-	if(step == board.length-4*board.length-4){
+	if(step == (board.length-4)*(board[r].length-4)+1){
 	    return true;
 	}
-	if(board[r][c]!=0){
-	    
+	if(board[r][c]<0){
 	    return false;
 	}
+	if(board[r][c]!=0){	    
+	  return false;
+	}
 	board[r][c]=step;
-	System.out.println(toString());
+	//System.out.println(toString());
 	if(solveH(r+2, c+1, step+1) ||
 	   solveH(r+2, c-1, step+1) ||
 	   solveH(r-2, c+1, step+1) ||
@@ -40,18 +42,12 @@ public class KnightBoard{
 	   solveH(r-1, c-2, step+1)){
 	    return true;
 	}else{
-	    return
-		solveH(r-2, c-1, step-1) ||
-		solveH(r-2, c+1, step-1) ||
-		solveH(r+2, c-1, step-1) ||
-		solveH(r+2, c+1, step-1) ||
-		solveH(r-1, c-2, step-1) ||
-		solveH(r-1, c+2, step-1) ||
-		solveH(r+1, c-2, step-1) ||
-		solveH(r+1, c+2, step-1);
+	    board[r][c]=0;
+	    step--;
+	    //System.out.println(toString());
+	return false;
 	}
-	
-	    }
+    }
     public boolean upRight(int r, int c, int step){
 	if(board[r][c]!=0){
 	    return false;
@@ -69,7 +65,7 @@ public class KnightBoard{
     public String toString(){
 	String ans="";
 	for(int r=2; r<board.length-2; r++){
-	    for(int c=2; c<board.length-2; c++){
+	    for(int c=2; c<board[r].length-2; c++){
 		ans+=board[r][c]+"\t";
 	    }
 	    ans+="\n";
