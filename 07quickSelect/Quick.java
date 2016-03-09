40e5ee36618c998a1f79e612eb75af380de1e3c5
Yiruz <2yi.z.ru2@gmail.com>
2016-03-08 20:44:05 -0500

    finish

--- 07quickSelect/Quick.java
+++ 07quickSelect/Quick.java
@@ -2,7 +2,7 @@ public class Quick{
     
     private static int partition(int[]data, int left, int right){
 	int ind = (int)(Math.random()*(right-left+1))+left;
-	System.out.println(ind);
+	//System.out.println(ind);
         int element = data[ind];
 	data[ind] = data[right];
 	data[right] = element;
@@ -22,6 +22,23 @@ public class Quick{
 	data[j+1] = element;
 	return j+1;
     }
+    public static int quickselect(int[]data, int k){
+	//System.out.println("k = "+k);
+	return quickselect(data,k,0,data.length-1);
+    }
+    public static int quickselect(int[]data,int k,int left,int right){
+	int ind = partition(data,left,right);
+	int val = 0;
+	if(k == ind){
+	    val = data[k];
+	}else if(k > ind){
+	    val = quickselect(data,k,ind+1,right);
+	}else if(k < ind){
+	    val = quickselect(data,k,left,ind-1);
+	}
+	return val;
+    }
+
 
     public static void print(int[]data){
 	String s = "[";
@@ -33,10 +50,10 @@ public class Quick{
     }
 
 
-    public static void main(String[]args){
+    /*public static void main(String[]args){
 	int[]data={-3,2,5,9,3,-5,34,0};
 	print(data);
-	System.out.println(partition(data,0,data.length-1));
+	System.out.println(quickselect(data,4));
 	print(data);
-    }
+	}*/
 }
