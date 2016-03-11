d25dd532534aa7a17c52ed9149a4d10b7b6cdc7a
Yiruz <2yi.z.ru2@gmail.com>
2016-03-11 12:34:10 -0500

    start working on alternate quick sort

--- 07quickSelect/Quick.java
+++ 07quickSelect/Quick.java
@@ -1,6 +1,7 @@
 public class Quick{
     
-    private static int partition(int[]data, int left, int right){
+    //Old*************************************************
+    private static int partitionOld(int[]data, int left, int right){
 	int ind = (int)(Math.random()*(right-left+1))+left;
 	//System.out.println(ind);
         int element = data[ind];
@@ -22,7 +23,59 @@ public class Quick{
 	data[j+1] = element;
 	return j+1;
     }
-    public static int quickselect(int[]data, int k){
+    public static int quickselectOld(int[]data, int k){
+	//System.out.println("k = "+k);
+	return quickselectOld(data,k,0,data.length-1);
+    }
+    public static int quickselectOld(int[]data,int k,int left,int right){
+	int ind = partitionOld(data,left,right);
+	int val = 0;
+	if(k == ind){
+	    val = data[k];
+	}else if(k > ind){
+	    val = quickselectOld(data,k,ind+1,right);
+	}else if(k < ind){
+	    val = quickselectOld(data,k,left,ind-1);
+	}
+	return val;
+    }
+    public static void quickSortOld(int[]data){
+	quickSortOld(data,0,data.length-1);
+    }
+    public static void quickSortOld(int[]data,int left,int right){
+	if(left<right){
+	    int ind = partitionOld(data,left,right);
+	    quickSortOld(data,ind+1,right);
+	    quickSortOld(data,left,ind-1);
+	}
+    }
+    //***************************************************
+
+    //new************************************************
+    private static int[] partition(int[]data, int left, int right){
+	int ind = (int)(Math.random()*(right-left+1))+left;
+	int count = 1;
+        int element = data[ind];
+	//data[ind] = data[right];
+	//data[right] = element;
+	int i = left;
+	int j = right;
+	while(i <= j){
+	    if(data[i] == element){
+		count++;
+	    }else if(data[i] > element){
+		swap(data, i, j);
+		j--;
+	    }else if(data[i] < element){
+		i++;
+	    }
+	}
+        int[]par = new int[2];
+	par[0] = i;
+	par[1] = j;
+	return par;
+    }
+    /*public static int quickselect(int[]data, int k){
 	//System.out.println("k = "+k);
 	return quickselect(data,k,0,data.length-1);
     }
@@ -47,14 +100,20 @@ public class Quick{
 	    quickSort(data,ind+1,right);
 	    quickSort(data,left,ind-1);
 	}
+	}*/
+
+    public static void swap(int[]data,int a,int b){
+	int temp = data[a];
+	data[a] = data[b];
+	data[b] = temp;
     }
 
     public static void print(int[]data){
 	String s = "[";
-	for(int i=0; i<data.length; i++){
+	for(int i=0; i<data.length-1; i++){
 	    s+=data[i]+", ";
 	}
-	s+="]";
+	s+=data[data.length-1]+"]";
 	System.out.println(s);
     }
 
@@ -62,11 +121,14 @@ public class Quick{
 	return "6,Zheng,Yiru";
     }
     public static void main(String[]args){
-	int[]data={-3,2,5,9,3,-5,34,0};
+	int[]data = new int[400000];
+	for(int i=0; i<data.length; i++){
+	    data[i] = (int)(Math.random()*5)-3;
+	}
+	print(data);
+	System.out.println(partition(data, 0, data.length-1));
+	print(data);
+	//quickSort(data);
 	//print(data);
-	//System.out.println(quickselect(data,0));
-	print(data);
-	quickSort(data);
-	print(data);
     }
 }
