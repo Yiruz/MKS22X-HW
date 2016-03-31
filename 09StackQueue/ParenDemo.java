3da0bfc3ca0bab2a869e531831b91c965eb8a0e3
Yiruz <2yi.z.ru2@gmail.com>
2016-03-30 22:06:43 -0400

    ismatching for stack

--- 09StackQueue/ParenDemo.java
+++ 09StackQueue/ParenDemo.java
@@ -3,20 +3,57 @@ public class ParenDemo{
     public static boolean isMatching(String s){
 	MyStack<Character> c = new MyStack<Character>();
 	for(int i=0; i<s.length(); i++){
-	    if(charAt(i)=='(' || charAt(i)=='[' || charAt(i)=='{' || charAt(i)=='<'){
-		c.push(i);
-	    }
-	    if(i == ')'){
-
+	    if(s.charAt(i)=='(' || s.charAt(i)=='[' || 
+	       s.charAt(i)=='{' || s.charAt(i)=='<'){
+		c.push(s.charAt(i));
+	    }else{
+		if(s.charAt(i) == ')'){
+		    if(c.peek() == '('){
+			c.pop();
+		    }else{
+			return false;
+		    }
+		}else
+		if(s.charAt(i) == ']'){
+		    if(c.peek() == '['){
+			c.pop();
+		    }else{
+			return false;
+		    }
+		}else
+		if(s.charAt(i) == '}'){
+		    if(c.peek() == '{'){
+			c.pop();
+		    }else{
+			return false;
+		    }
+		}else
+		if(s.charAt(i) == '>'){
+		    if(c.peek() == '<'){
+			c.pop();
+		    }else{
+			return false;
+		    }
+		}
+		if(c.isEmpty() && (s.charAt(i)==')' || s.charAt(i)==']' || 
+				   s.charAt(i)=='}' || s.charAt(i)=='>')){
+		    System.out.println("closing without opening");
+		    return false;
+		    
+		}
 	    }
 	}
+	return c.isEmpty();
     }
 
     public static void main(String[]args){
-	String input = "";
+	String input = "()()(([[]]))";
 	if(args.length > 0){
 	    input = args[0];
-	    System.out.println(isMatching(input));
+	    System.out.println( isMatching(input)); 
+	}else{
+	    System.out.println("Usage:"); 
+	    System.out.println("java ParenDemo \"text\""); 
 	}
 	
     }
