3fff29fa703a46a21398cb197ea3aebeea091062
Yiruz <2yi.z.ru2@gmail.com>
2016-03-31 18:56:57 -0400

    test cases

--- 09StackQueue/Driver.java
+++ 09StackQueue/Driver.java
@@ -3,11 +3,34 @@ public class Driver{
 	MyStack<Integer> s = new MyStack<Integer>();
 	MyQueue<Integer> q = new MyQueue<Integer>();
 
-	for(int i=0; i<50; i++){
+	for(int i=0; i<20; i++){
 	    s.push(i);
-	    q.push(i);
+	    q.enqueue(i);
 	}
-	for
+	s.print();
+	q.print();
+
+	//test remove
+	s.pop();
+	q.dequeue();
+	s.pop();
+	q.dequeue();
+	s.pop();
+	q.dequeue();
+	s.pop();
+	q.dequeue();
+	s.print();
+	q.print();
+	System.out.println("-----pop 4 times --------------------");
+
+	//test add
+	s.push(-1);
+	q.enqueue(-1);
+	s.push(-2);
+	q.enqueue(-2);
+	s.print();
+	q.print();
+	
     }
 
 }
