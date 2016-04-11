8b598c246a02662500da2252fe84a7f1d1e167a9
Yiruz <2yi.z.ru2@gmail.com>
2016-04-11 01:29:36 -0400

    work on stack, compiles

--- 11frontier/FrontierStack.java
+++ 11frontier/FrontierStack.java
@@ -1,6 +1,18 @@
+import java.util.*;
+
 public class FrontierStack<T> implements Frontier<T>{
     /***Make This Work This Weekend!***/
     /***You can use your classes or built in ones***/
     /***You can extend another class OR wrap around it***/
+    public Stack<T> data;
 
+    public void add(T element){
+	data.push(element);
+    }
+    public T next(){
+	return data.pop();
+    }
+    public boolean hasNext(){
+	return !data.empty();
+    }
 }
