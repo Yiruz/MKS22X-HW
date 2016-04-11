8679032cc03ba1f07c07bc8a0cdbb85d6bbe7406
Yiruz <2yi.z.ru2@gmail.com>
2016-04-11 12:21:33 -0400

    write frontier queue

--- 11frontier/FrontierQueue.java
+++ 11frontier/FrontierQueue.java
@@ -1,6 +1,18 @@
+import java.util.*;
+
 public class FrontierQueue<T> implements Frontier<T>{
     /***Make This Work This Weekend!***/
     /***You can use your classes or built in ones***/
     /***You can extend another class OR wrap around it***/
+    public Queue<T> q;
 
+    public void add(T element){
+	q.add(element);
+    }
+    public T next(){
+	return q.remove();
+    }
+    public boolean hasNext(){
+	return q.peek() != null;
+    }
 }
