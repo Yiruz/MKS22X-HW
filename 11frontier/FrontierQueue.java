63f3691bc4ac6ace4bf63e0b72b74e778fcd8b5e
Yiruz <2yi.z.ru2@gmail.com>
2016-04-12 20:06:19 -0400

    add constructor

--- 11frontier/FrontierQueue.java
+++ 11frontier/FrontierQueue.java
@@ -4,8 +4,11 @@ public class FrontierQueue<T> implements Frontier<T>{
     /***Make This Work This Weekend!***/
     /***You can use your classes or built in ones***/
     /***You can extend another class OR wrap around it***/
-    public Queue<T> q;
+    Queue<T> q;
 
+    public FrontierQueue(){
+	q = new LinkedList<T>();
+    }
     public void add(T element){
 	q.add(element);
     }
--- 11frontier/FrontierStack.java
+++ 11frontier/FrontierStack.java
@@ -4,7 +4,11 @@ public class FrontierStack<T> implements Frontier<T>{
     /***Make This Work This Weekend!***/
     /***You can use your classes or built in ones***/
     /***You can extend another class OR wrap around it***/
-    public Stack<T> data;
+    Stack<T> data;
+    
+    public FrontierStack(){
+	data = new Stack<T>();
+    }
 
     public void add(T element){
 	data.push(element);
