6b392166544e78620d261db4cb1b22605137f9ea
Yiruz <2yi.z.ru2@gmail.com>
2016-04-14 00:10:08 -0400

    write solution coordinates

--- 11frontier/BetterMaze.java
+++ 11frontier/BetterMaze.java
@@ -19,9 +19,6 @@ public class BetterMaze{
 	    xy[1] = y;
 	    prev = p;
 	}
-	public void setPrev(Node n){
-	    prev = n;
-	}
 	public Node getPrev(){
 	    return prev;
 	}
@@ -38,6 +35,8 @@ public class BetterMaze{
     private int      startRow,startCol;
     private Frontier<Node> placesToGo;
     private boolean  animate;
+    private Node end;
+    
 
     /**return a copy of solution.
       This should be : [x1,y1,x2,y2,x3,y3...]
@@ -47,7 +46,17 @@ public class BetterMaze{
       *Postcondition:  the correct solution is in the returned array
       **/
     public int[] solutionCoordinates(){   
-        /** IMPLEMENT THIS **/      
+        /** IMPLEMENT THIS **/
+	ArrayList<Integer>data = new ArrayList<Integer>();
+	while(end != null){
+	    data.add(end.getRow());
+	    data.add(end.getCol());
+	    end = end.getPrev();
+	}
+	solution = new int[data.size()];
+	for(int i=0; i<data.size(); i++){
+	    solution[i] = data.get(i);
+	}
 	return solution;
     }    
 
@@ -81,137 +90,125 @@ public class BetterMaze{
 	    int row = head.getRow();
 	    int col = head.getCol();
 	    if(maze[row][col] == 'E'){
+		end = head;
 		return true;
 	    }
 	    if(moveable(col+1,row)){
-		Node n = new Node(col+1,row);
-		placesToGo.add(n);
-		n.setPrev(head);
+		placesToGo.add(new Node(col+1,row,head));
 	    }
 	    if(moveable(col-1,row)){
-		Node n = new Node(col-1,row);
-		placesToGo.add(n);
-		n.setPrev(head);
+		placesToGo.add(new Node(col-1,row,head));
 	    }
 	    if(moveable(col,row-1)){
-		Node n = new Node(col,row-1);
-		placesToGo.add(n);
-		n.setPrev(head);
+		placesToGo.add(new Node(col,row-1,head));
 	    }
 	    if(moveable(col,row+1)){
-		Node n = new Node(col,row+1);
-		placesToGo.add(n);
-		n.setPrev(head);
+		placesToGo.add(new Node(col,row+1,head));
 	    }
-	    maze[row][col] = '.';
-	}
-	return false;
-    }    
-     
-    private boolean moveable(int col, int row){
-	char c = maze[row][col];
-	return c == ' ' || c == 'E';
-    }
+	     maze[row][col] = '.';
+	 }
+	 return false;
+     }    
 
-   /**mutator for the animate variable  **/
-    public void setAnimate(boolean b){
-	animate = b;
-    }    
+     private boolean moveable(int col, int row){
+	 char c = maze[row][col];
+	 return c == ' ' || c == 'E';
+     }
 
-
-    public BetterMaze(String filename){
-	animate = false;
-	int maxc = 0;
-	int maxr = 0;
-	startRow = -1;
-	startCol = -1;
-	//read the whole maze into a single string first
-	String ans = "";
-	try{
-	    Scanner in = new Scanner(new File(filename));
-
-	    //keep reading next line
-	    while(in.hasNext()){
-		String line = in.nextLine();
-		if(maxr == 0){
-		    //calculate width of the maze
-		    maxc = line.length();
-		}
-		//every new line add 1 to the height of the maze
-		maxr++;
-		ans += line;
-	    }
-	}
-	catch(Exception e){
-	    System.out.println("File: " + filename + " could not be opened.");
-	    e.printStackTrace();
-	    System.exit(0);
-	}
-	System.out.println(maxr+" "+maxc);
-	maze = new char[maxr][maxc];
-	for(int i = 0; i < ans.length(); i++){
-	    char c = ans.charAt(i);
-	    maze[i / maxc][i % maxc] = c;
-	    if(c == 'S'){
-		startCol = i % maxc;
-		startRow = i / maxc;
-	    }
-	}
-    }
+    /**mutator for the animate variable  **/
+     public void setAnimate(boolean b){
+	 animate = b;
+     }    
 
 
+     public BetterMaze(String filename){
+	 animate = false;
+	 int maxc = 0;
+	 int maxr = 0;
+	 startRow = -1;
+	 startCol = -1;
+	 //read the whole maze into a single string first
+	 String ans = "";
+	 try{
+	     Scanner in = new Scanner(new File(filename));
+
+	     //keep reading next line
+	     while(in.hasNext()){
+		 String line = in.nextLine();
+		 if(maxr == 0){
+		     //calculate width of the maze
+		     maxc = line.length();
+		 }
+		 //every new line add 1 to the height of the maze
+		 maxr++;
+		 ans += line;
+	     }
+	 }
+	 catch(Exception e){
+	     System.out.println("File: " + filename + " could not be opened.");
+	     e.printStackTrace();
+	     System.exit(0);
+	 }
+	 System.out.println(maxr+" "+maxc);
+	 maze = new char[maxr][maxc];
+	 for(int i = 0; i < ans.length(); i++){
+	     char c = ans.charAt(i);
+	     maze[i / maxc][i % maxc] = c;
+	     if(c == 'S'){
+		 startCol = i % maxc;
+		 startRow = i / maxc;
+	     }
+	 }
+     }
 
 
+     private static final String CLEAR_SCREEN =  "\033[2J";
+     private static final String HIDE_CURSOR =  "\033[?25l";
+     private static final String SHOW_CURSOR =  "\033[?25h";
+     private String go(int x,int y){
+	 return ("\033[" + x + ";" + y + "H");
+     }
+     private String color(int foreground,int background){
+	 return ("\033[0;" + foreground + ";" + background + "m");
+     }
+
+     public void clearTerminal(){
+	 System.out.println(CLEAR_SCREEN);
+     }
+
+     public void wait(int millis){
+	 try {
+	     Thread.sleep(millis);
+	 }
+	 catch (InterruptedException e) {
+	 }
+     }
 
 
-
-    private static final String CLEAR_SCREEN =  "\033[2J";
-    private static final String HIDE_CURSOR =  "\033[?25l";
-    private static final String SHOW_CURSOR =  "\033[?25h";
-    private String go(int x,int y){
-	return ("\033[" + x + ";" + y + "H");
-    }
-    private String color(int foreground,int background){
-	return ("\033[0;" + foreground + ";" + background + "m");
-    }
-
-    public void clearTerminal(){
-	System.out.println(CLEAR_SCREEN);
-    }
-
-    public void wait(int millis){
-	try {
-	    Thread.sleep(millis);
-	}
-	catch (InterruptedException e) {
-	}
-    }
-
-
-    public String toString(){
-	int maxr = maze.length;
-	int maxc = maze[0].length;
-	String ans = "";
-	if(animate){
-	    ans = "Solving a maze that is " + maxr + " by " + maxc + "\n";
-	}
-	for(int i = 0; i < maxc * maxr; i++){
-	    if(i % maxc == 0 && i != 0){
-		ans += color(37,40) + "\n";
-	    }
-	    char c =  maze[i / maxc][i % maxc];
-	    if(c == '#'){
-		ans += color(38,47)+c;
-	    }else{
-		ans += color(33,40)+c;
-	    }
-	}
-	//nice animation string
-	if(animate){
-	    return HIDE_CURSOR + go(0,0) + ans + color(37,40) +"\n"+ SHOW_CURSOR + color(37,40);
-	}else{
-	    return ans + color(37,40) + "\n";
-	}
+     public String toString(){
+	 int maxr = maze.length;
+	 int maxc = maze[0].length;
+	 String ans = "";
+	 if(animate){
+	     ans = "Solving a maze that is " + maxr + " by " + maxc + "\n";
+	 }
+	 for(int i = 0; i < maxc * maxr; i++){
+	     if(i % maxc == 0 && i != 0){
+		 ans += color(37,40) + "\n";
+	     }
+	     char c =  maze[i / maxc][i % maxc];
+	     if(c == '#'){
+		 ans += color(38,47)+c;
+	     }else{
+		 ans += color(33,40)+c;
+	     }
+	 }
+	 //nice animation string
+	 if(animate){
+	     return HIDE_CURSOR + go(0,0) + ans + color(37,40) +"\n"+ SHOW_CURSOR + color(37,40);
+	 }else{
+	     return ans + color(37,40) + "\n";
+	 }
     } 
     
 }
