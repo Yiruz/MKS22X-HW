e71cfd138347cba41b2509e78c2c25040c4ecd88
Yiruz <2yi.z.ru2@gmail.com>
2016-04-21 12:33:10 -0400

    work on bstree

--- 12bstree/BSTree.java
+++ 12bstree/BSTree.java
@@ -28,13 +28,36 @@ public class BSTree<T extends comparable<T>>{
 	public void setRight(Node r){
 	    right = r;
 	}
-    }
-    public String toString(){
-	String s = "";
-	if(left != null){
-	    s+=left.toString();
+	public String toString(){
+	    String s = "";
+	    if(left != null){
+		s+=left.toString();
+	    }
+	    s+=data+" ";
+	    if(right != null){
+		s+=right.toString();
+	    }
+	    return s;
 	}
-	return s;
+	public int height(){
+	    int h = 0;
+	    if(left==null && right==null){
+		h = 1;
+	    }else{
+		h = 1+Math.max(left.height(),right.height());
+	    }
+	    return h;
+	}
+	public void add(T value){
+	    if(data.compareTo(value)<0){
+		Node n = new Node(value,null,null);
+	    }
+	}
+	public boolean contains(T value){
+	    if(data < value){
+		
+	    }
+	    return false;
+	}
     }
-
 }
