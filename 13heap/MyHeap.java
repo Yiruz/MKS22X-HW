39b409d1165ca199a8e71bf4f20c5e69e4b58ca1
Yiruz <2yi.z.ru2@gmail.com>
2016-05-09 20:21:41 -0400

    fix pushdown

--- 13heap/MyHeap.java
+++ 13heap/MyHeap.java
@@ -5,52 +5,58 @@ import java.util.*;
     private T[] data;
     private int size;
     private boolean max;
-    
+    private boolean debug = true;
+
     public MyHeap(){
-	data = (T[])new Comparable[1];
+	data = (T[])new Comparable[10];
 	size = 0;
+	max = true;
     }
     public MyHeap(T[] ary){
-	data = (T[])new Comparable[ary.length];
-	for(int i=1; i<ary.length; i++){
-	    data[i] = ary[i];
-	}
 	size = ary.length;
+	data = (T[])new Comparable[size+1];
+	for(int i=0; i<ary.length; i++){
+	    data[i+1] = ary[i];
+	}
+	max = true;
+	heapify();
     }
-    /*public MyHeap(boolean isMax){
-      max = isMax;
-      }
-      public MyHeap(T[] ary, boolean isMax){
-      
-      }*/
-    
+    public MyHeap(boolean isMax){
+	max = isMax;
+    }
+    public MyHeap(T[] ary, boolean isMax){
+	this(ary);
+	max = isMax;
+    }
+    private void swap(int a, int b){
+	T temp = data[a];
+	data[a] = data[b];
+	data[b] = temp;
+    }
+
     private void pushDown(int k){
-	if(data[k].compareTo(data[k*2])<0 || data[k].compareTo(data[k*2+1])<0){
-	    if(data[k*2].compareTo(data[k*2+1]) > 0){
-		T temp = data[k];
-		data[k] = data[k*2];
-		data[k*2] = temp;
-		pushDown(k*2);
-	    }else{
-		T temp = data[k];
-		data[k] = data[k*2+1];
-		data[k*2+1] = temp;
-		pushDown(k*2+1);
+	if(k*2+1 <= size){
+	    if(compare(k,k*2) ||  compare(k,k*2+1)){	
+		if(compare(k*2,k*2+1)){
+		    swap(k, k*2+1);
+		    pushDown(k*2+1);
+		}else{
+		    swap(k, k*2);
+		    pushDown(k*2);
+		}
+	    }
+	    if(debug){
+		System.out.println(toString());
+		
 	    }
 	}
     }
+
     private void pushUp(int k){
-	if(data[k].compareTo(data[k/2])>0 || data[k].compareTo(data[k/2+1])>0){	
-	    if(data[k/2].compareTo(data[k/2+1]) > 0){
-		T temp = data[k];
-		data[k] = data[k/2];
-		data[k/2] = temp;
+	if(k/2 >= 1){
+	    if(compare(k,k/2)){	
+		swap(k, k/2);
 		pushUp(k/2);
-	    }else{
-		T temp = data[k];
-		data[k] = data[k/2+1];
-		data[k/2+1] = temp;
-		pushUp(k/2+1);
 	    }
 	}
     }
@@ -59,20 +65,26 @@ import java.util.*;
 	    pushDown(i);
 	}
     }
+    public T peek(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
+	return data[1];
+    }
     public T delete(){
 	if(size == 0){
 	    throw new NoSuchElementException();
 	}
 	T temp = data[1];
-	for(int i=1; i<size; i++){
+	for(int i=1; i<size+1; i++){
 	    data[i] = data[i+1];
 	}
-	//data[size+1] = ;
+	data[size] = null;
 	size--;
 	return temp;
     }
     public void add(T x){
-	if(data.length <= size){
+	if(data.length <= size+1){
 	    doubleSize();
 	}
 	data[size] = x;
@@ -92,6 +104,16 @@ import java.util.*;
 	for(int i=1; i<size; i++){
 	    ret += data[i] + ", ";
 	}
-	return ret + data[size];
+	return ret + data[size] + "]";
+	
+	//return Arrays.toString(data);
+    }
+
+    private boolean compare(int a, int b){
+	if(max){
+	    return data[a].compareTo(data[b]) > 0; 
+	}else{
+	    return data[a].compareTo(data[b]) < 0; 
+	}
     }
 }
